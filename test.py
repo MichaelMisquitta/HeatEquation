2b95a0c23b1b1c5bcea5ffbca7bc48b505288434
Michael Misquitta <michaelmisquitta@gmail.com>
2026-05-09 19:37:04 -0500

    tett file

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 import numpy as np
-nodes = np.array([0, 1, 3])
-nodest = np.array([0, 1, 2])
-testmat = np.array([[1, 9, 5, 3],[1, 2, 5, 6],[1, 4, 7, 6],[7, 1, 5, 6]])
-print(testmat)
-indices = np.ix_(nodes,nodest)
-testmat[indices] = np.zeros((3,3))
-print(testmat)
+from helper import calc_tringle_area
+
+coords = np.array([[0,0,0],[1,4,5],[1,2,3]])
+print(calc_tringle_area(coords))
 #print(indices)
